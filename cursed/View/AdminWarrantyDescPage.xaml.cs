c8ca67685e338457fb2f831c588f97e2da30d7b8
burdukow <brdkv19@gmail.com>
2024-06-02 12:09:49 +0500

    Added edit existing warranty (Order) functionality

--- cursed/View/AdminWarrantyDescPage.xaml.cs
+++ cursed/View/AdminWarrantyDescPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using cursed.Model;
+using cursed.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
@@ -13,10 +16,12 @@ namespace cursed.View
     public partial class AdminWarrantyDescPage : Page
     {
         Core db = new Core();
+        private Orders currentOrder;
         public AdminWarrantyDescPage(Orders order)
         {
 
             InitializeComponent();
+            currentOrder = order;
             ComputerNameTextBlock.Text = db.context.Computers.Where(x => x.ComputerId == order.ComputerId).FirstOrDefault().ComputerName;
             ComputerComponentTextBlock.Text = "Проблема с: " +
                 db.context.Components.Where(x => x.IdComponent == order.ComponentId).FirstOrDefault().ComponentTypes.ComponentTypeName + " " +
@@ -29,14 +34,40 @@ namespace cursed.View
             {
                 fio[i] += users[i].LastName + " " + users[i].FirstName[0] + ". " + users[i].Patronymic[0] + ". ";
             }
-            MasterComboBox.ItemsSource = fio;
+            MasterComboBox.ItemsSource = db.context.Users.Where(x => x.RoleId == 2).ToList();
+            MasterComboBox.DisplayMemberPath = "LastName";
+            MasterComboBox.SelectedValuePath = "IdUser";
             List<OrderStatuses> orderStatuses = new List<OrderStatuses>();
             orderStatuses = db.context.OrderStatuses.ToList();
             OrderStatusComboBox.ItemsSource = orderStatuses;
             Computers computer = db.context.Computers.Where(x => x.ComputerId == order.ComputerId).FirstOrDefault();
             string filename = computer.ComputerPicPath;
-            string compImagePath = @"../../Resources/Images/Computers/" + filename;
-            ComputerImage.Source = new BitmapImage(new Uri(compImagePath, UriKind.Relative));
+            ComputerImage.Source = new BitmapImage(new Uri(filename, UriKind.RelativeOrAbsolute));
+        }
+
+        private void SaveWarrantyInfo(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+
+                try
+                {
+                int statusId = (OrderStatusComboBox.SelectedItem as OrderStatuses)?.IdOrderStatus ?? 0;
+                DateTime date = OrderDateExecDatePicker.SelectedDate ?? DateTime.Now;
+                int? masterId = (int)(MasterComboBox.SelectedValue ?? Properties.Settings.Default.idUser);
+                    Orders editedOrder = db.context.Orders.Where(x => x.IdOrder == currentOrder.IdOrder).FirstOrDefault();
+                    editedOrder.OrderStatusId = statusId;
+                    editedOrder.MasterId = masterId;
+                    editedOrder.OrderExecDate = date;
+                    db.context.SaveChanges();
+                    MessageBox.Show("Информация о заявке успешно отредактирована!");
+                    this.NavigationService.GoBack();
+                }
+                catch
+                {
+                    MessageBox.Show("Во время редактирования информации произошла ошибка!");
+                }
+
+
         }
     }
 }
