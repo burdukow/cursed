436f70f0c5485f424f90aa389d2d879d80082bd7
burdukow <brdkv19@gmail.com>
2024-06-03 17:18:06 +0500

    Added generate in word file info

--- cursed/View/WarrantyPage.xaml.cs
+++ cursed/View/WarrantyPage.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using cursed.Model;
 using cursed.ViewModel;
+using Microsoft.Office.Interop.Word;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Markup;
 using System.Windows.Media.Imaging;
 
 namespace cursed.View
@@ -13,10 +16,11 @@ namespace cursed.View
     /// <summary>
     /// Логика взаимодействия для WarrantyPage.xaml
     /// </summary>
-    public partial class WarrantyPage : Page
+    public partial class WarrantyPage : System.Windows.Controls.Page
     {
         Core db = new Core();
         List<Orders> orderList = new List<Orders>();
+        Orders currOrder = new Orders();
         public WarrantyPage()
         {
             InitializeComponent();
@@ -25,7 +29,7 @@ namespace cursed.View
 
         private void CodeWarrantyButtonCLick(object sender, System.Windows.RoutedEventArgs e)
         {
-            if(orderList.Where(x => x.OrderCode == CodeTextBox.Text).FirstOrDefault() != null)
+            if (orderList.Where(x => x.OrderCode == CodeTextBox.Text).FirstOrDefault() != null)
             {
                 Orders order = orderList.Where(x => x.OrderCode == CodeTextBox.Text).First();
                 ShowInfo(order);
@@ -37,6 +41,7 @@ namespace cursed.View
         }
         private void ShowInfo(Orders order)
         {
+            currOrder = order;
             WarrantyCodeStackPanel.Visibility = System.Windows.Visibility.Collapsed;
             WarrantyInfoStackPanel.Visibility = System.Windows.Visibility.Visible;
             ComputerNameTextBlock.Text = db.context.Computers.Where(x => x.ComputerId == order.ComputerId).FirstOrDefault().ComputerName;
@@ -46,14 +51,45 @@ namespace cursed.View
                 db.context.Components.Where(x => x.IdComponent == order.ComponentId).FirstOrDefault().ComponentModel;
             WarrantyDescTextBlock.Text = "Описание:\n" + order.OrderDescription;
             string master = db.context.Users.Where(x => x.IdUser == order.MasterId).FirstOrDefault() == null ? null : db.context.Users.Where(x => x.IdUser == order.MasterId).FirstOrDefault().LastName + " " + db.context.Users.Where(x => x.IdUser == order.MasterId).FirstOrDefault().FirstName;
-            MasterTextBlock.Text = master != null? master : "Мастер не назначен";
+            MasterTextBlock.Text = master != null ? master : "Мастер не назначен";
             string orderStatus = db.context.OrderStatuses.Where(x => x.IdOrderStatus == order.OrderStatusId).Select(x => x.OrderStatusName).First();
             OrderStatusTextBlock.Text = orderStatus;
+            if (orderStatus == "Готов, ожидает клиента")
+                GenerateWarrantyButton.Visibility = System.Windows.Visibility.Visible;
             string orderExecDate = order.OrderExecDate.ToString();
             OrderDateExecTextBlock.Text = orderExecDate != null ? orderExecDate : "Дата не указана";
             Computers computer = db.context.Computers.Where(x => x.ComputerId == order.ComputerId).FirstOrDefault();
             string filename = computer.ComputerPicPath;
             ComputerImage.Source = new BitmapImage(new Uri(filename, UriKind.RelativeOrAbsolute));
         }
-    }
+
+        private void GenerateWarrantyButtonCLick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult dialog = MessageBox.Show("Желаете сохранить гарантийный талон?", "Гарантийный талон", MessageBoxButton.YesNo);
+            if (dialog == MessageBoxResult.Yes)
+            {
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.FileName = "Гарантийный талон";
+                dlg.DefaultExt = ".txt";
+                dlg.Filter = "Office Files|*.doc;*.docx";
+                Nullable<bool> result = dlg.ShowDialog();
+                var warrantyData = new Dictionary<string, string>
+                {
+                    {"<IdOrder>", currOrder.IdOrder.ToString()},
+                    {"<PcName>", db.context.Computers.Where(x => x.ComputerId == currOrder.ComputerId).FirstOrDefault().ComputerName},
+                    {"<ModelName>", db.context.Components.Where(x => x.IdComponent == currOrder.ComponentId).FirstOrDefault().ComponentModel},
+                    {"<Article>", db.context.Components.Where(x => x.IdComponent == currOrder.ComponentId).FirstOrDefault().ComponentArticle},
+                    {"<WarrantyExecDate>", currOrder.OrderExecDate.Value .ToString("dd.MM.yyyy")},
+                    {"<Master>", db.context.Users.Where(x => x.IdUser == currOrder.MasterId).FirstOrDefault().LastName + " " + db.context.Users.Where(x => x.IdUser == currOrder.MasterId).FirstOrDefault().FirstName},
+                    {"<ComponentPrice>", db.context.Components.Where(x => x.IdComponent == currOrder.ComponentId).FirstOrDefault().ComponentPrice.ToString() + " руб." },
+                    {"<Description>", currOrder.OrderDescription}
+                };
+                if (result == true)
+                {
+                    FilesVM.EditWordFile(dlg.FileName, warrantyData);
+                }
+            }
+           
+        }
+}
 }
--- cursed/ViewModel/FilesVM.cs
+++ cursed/ViewModel/FilesVM.cs
@@ -1,13 +1,77 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace cursed.ViewModel
 {
     public class FilesVM
     {
+
+        /// <summary>
+        /// Редактирует шаблон Word-документа, заменяя заполнители данными из словаря.
+        /// </summary>
+        /// <param name="outputPath">Путь для сохранения отредактированного документа.</param>
+        /// <param name="data">Словарь, содержащий пары "ключ-значение", где ключ - имя заполнителя в шаблоне, а значение - данные для замены.</param>
+        /// <remarks>
+        /// Метод открывает шаблон Word-документа, ищет заполнители, соответствующие ключам в словаре, 
+        /// заменяет их значениями и сохраняет отредактированный документ в указанный путь.
+        /// </code>
+        /// </remarks>
+        /// <exception cref="Exception">
+        /// Выбрасывается, если возникла ошибка при работе с Word-документом.
+        /// </exception>
+public static void EditWordFile(string outputPath, Dictionary<string, string> data)
+        {
+            string baseDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string relativeTemplatePath = @"..\..\Resources\Docs\WarrantyTemplate.docx";
+            string templatePath = Path.Combine(baseDirectory, relativeTemplatePath);
+
+            try
+            {
+                var app = new Microsoft.Office.Interop.Word.Application();
+                app.Documents.Open(templatePath);
+
+
+                Object missing = Type.Missing;
+
+                foreach ( var item in data) { 
+                    Find find = app.Selection.Find;
+                    find.Text = item.Key;
+                    find.Replacement.Text = item.Value;
+
+                    Object wrap = WdFindWrap.wdFindContinue;
+                    Object replace = WdReplace.wdReplaceAll;
+
+                    find.Execute(FindText: Type.Missing,
+                        MatchCase: false,
+                        MatchWholeWord: false,
+                        MatchWildcards: false,
+                        MatchSoundsLike: missing,
+                        MatchAllWordForms: false,
+                        Forward: true,
+                        Wrap: wrap,
+                        Format: false,
+                        ReplaceWith: missing, Replace: replace);
+                }
+
+                app.ActiveDocument.SaveAs2(outputPath);
+                app.ActiveDocument.Close();
+                app.Quit();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
         private const string ApiKey = ""; // Сюда надо APIKey от imgbb 
         private const string BaseUrl = "https://api.imgbb.com/1/upload";
 
